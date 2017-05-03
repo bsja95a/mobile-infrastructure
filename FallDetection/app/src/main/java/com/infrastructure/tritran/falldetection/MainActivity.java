4fb1394ffc4f72d8e51afd35186881baa87dae0f
Tri Tran <tvtri96@yahoo.com.vn>
2017-05-03 17:20:23 +0200

    Send email when the phone falls

--- FallDetection/app/src/main/java/com/infrastructure/tritran/falldetection/MainActivity.java
+++ FallDetection/app/src/main/java/com/infrastructure/tritran/falldetection/MainActivity.java
@@ -5,14 +5,27 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    private final String username = "henrycity23@gmail.com";
+    private final String password = "henryhustle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +43,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             double x = sensorEvent.values[0];
             double y = sensorEvent.values[1];
             double z = sensorEvent.values[2];
-            double vLength = Math.sqrt(Math.pow(x,2)+Math.pow(y,2)+Math.pow(z,2));
+            double vLength = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2));
             if (vLength < 2) {
-                // TODO: Send email here
                 Toast.makeText(this, "Fall detected", Toast.LENGTH_SHORT).show();
+                if (isOnline()) {
+                    new SendEmailTask().execute();
+                }
             }
         }
     }
@@ -53,6 +68,47 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onPause() {
         super.onPause();
         mSensorManager.unregisterListener(this);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    private class SendEmailTask extends AsyncTask<Void, Integer, Boolean> {
+        protected Boolean doInBackground(Void... urls) {
+            Properties props = new Properties();
+            props.put("mail.smtp.auth", "true");
+            props.put("mail.smtp.starttls.enable", "true");
+            props.put("mail.smtp.host", "smtp.gmail.com");
+            props.put("mail.smtp.port", "587");
+
+            Session session = Session.getInstance(props,
+                    new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                            return new PasswordAuthentication(username, password);
+                        }
+                    });
+            try {
+                Message message = new MimeMessage(session);
+                message.setFrom(new InternetAddress("henrycity23@gmail.com"));
+                message.setRecipients(Message.RecipientType.TO,
+                        InternetAddress.parse("tvtri96@gmail.com"));
+                message.setSubject("Fallen Android!");
+                message.setText("Your location: ");
+                Transport.send(message);
+                System.out.println("Send");
+                return true;
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        protected void onPostExecute(Boolean result) {
+        }
     }
 
 }
